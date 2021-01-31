ec61356ea4539bcb0997294ff89fc50ace22d753
Victor Yao <55715390+VictorVy@users.noreply.github.com>
2021-01-31 11:18:34 -0800

    Add files via upload

--- S2/_2018_Sunflowers.java
+++ S2/_2018_Sunflowers.java
@@ -1,87 +1,69 @@
-//solved
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class _2018_Sunflowers
 {
-    static int flowers;
-    static int[][] measurements;
+    static int n;
+    static int[][] data;
 
     public static void main(String[] args) throws IOException
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        flowers = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
+        data = new int[n][n];
 
-        measurements = new int[flowers][flowers];
-
-        for(int i = 0; i < flowers; i++)
+        for(int i = 0; i < n; i++)
         {
-            String[] row = br.readLine().split(" ");
+            st = new StringTokenizer(br.readLine());
 
-            for(int k = 0; k < flowers; k++)
-                measurements[i][k] = Integer.parseInt(row[k]);
+            for(int k = 0; k < n; k++)
+                data[i][k] = Integer.parseInt(st.nextToken());
         }
 
         for(int i = 0; i < 4; i++)
         {
-            if(check())
+            if(checkData() && data[0][0] < data[1][0])
             {
-                print();
+                printData();
                 break;
             }
-            else
-                rotate();
+
+            rotateData();
         }
     }
 
-    private static boolean check()
+    static boolean checkData()
     {
-        int rowInit = 0;
-
-        for(int i = 0; i < flowers; i++)
-        {
-            //checks order of rows
-            if(rowInit > measurements[i][0])
-                return false;
-            else
-                rowInit = measurements[i][0];
-
-            for(int k = 0; k < flowers - 1; k++)
-            {
-                //checks order of measurements
-                if(measurements[i][k] > measurements[i][k + 1])
-                    return false;
-            }
-        }
+        for(int i = 0; i < n; i++)
+            for(int k = 0; k < n - 1; k++)
+                if(data[i][k] > data[i][k + 1]) return false;
 
         return true;
     }
 
-    private static void rotate()
+    static void printData()
     {
-        int[][] rotated = new int[flowers][flowers];
-
-        //assigns each row of measurements to column of new array
-        for(int i = 0; i < flowers; i++)
+        for(int i = 0; i < n; i++)
         {
-            for(int k = 0; k < flowers; k++)
-                rotated[k][(flowers - 1) - i] = measurements[i][k];
-        }
-
-        measurements = rotated;
-    }
-
-    private static void print()
-    {
-        for(int i = 0; i < flowers; i++)
-        {
-            for(int k = 0; k < flowers; k++)
-                System.out.print(measurements[i][k] + " ");
+            for(int k = 0; k < n; k++)
+                System.out.print(data[i][k] + " ");
 
             System.out.print("\n");
         }
+    }
+
+    static void rotateData()
+    {
+        int[][] rotated = new int[n][n];
+
+        for(int i = 0; i < n; i++)
+            for(int k = 0; k < n; k++)
+                rotated[k][n - i - 1] = data[i][k];
+
+        data = rotated;
     }
 }
