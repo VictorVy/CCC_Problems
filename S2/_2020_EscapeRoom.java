6ab3192eb3b249c425f864f9972186c4e425b317
Victor Yao <55715390+VictorVy@users.noreply.github.com>
2022-02-15 23:20:27 -0800

    Update _2020_EscapeRoom.java

--- S2/_2020_EscapeRoom.java
+++ S2/_2020_EscapeRoom.java
@@ -6,9 +6,12 @@ import java.util.StringTokenizer;
 
 public class _2020_EscapeRoom
 {
-    static int r, c;
-    static ArrayList<ArrayList<int[]>> cells;
-    static boolean[][] covered;
+    private static int r, c;
+
+    private static int[][] room;
+    private static boolean[][] visited;
+
+    private static ArrayList<ArrayList<int[]>> factors = new ArrayList<>();
 
     public static void main(String[] args) throws IOException
     {
@@ -17,36 +20,36 @@ public class _2020_EscapeRoom
 
         r = Integer.parseInt(br.readLine());
         c = Integer.parseInt(br.readLine());
-
-        cells = new ArrayList<>(1000001);
-        covered = new boolean[r][c];
+        room = new int[r][c];
+        visited = new boolean[r][c];
 
         for(int i = 0; i < 1000001; i++)
-            cells.add(new ArrayList<>());
+            factors.add(new ArrayList<>());
 
-        for(int y = 1; y <= r; y++)
+        for(int y = 0; y < r; y++)
         {
             st = new StringTokenizer(br.readLine());
 
-            for(int x = 1; x <= c; x++)
-                cells.get(Integer.parseInt(st.nextToken())).add(new int[]{y, x});
+            for(int x = 0; x < c; x++)
+            {
+                room[y][x] = Integer.parseInt(st.nextToken());
+                int n = (y + 1) * (x + 1);
+                factors.get(n).add(new int[] {(y + 1), (x + 1)});
+            }
         }
 
-        System.out.println(escapable(r, c) ? "yes" : "no");
+        System.out.println(search(0, 0) ? "yes" : "no");
     }
 
-    static boolean escapable(int y, int x)
+    private static boolean search(int y, int x)
     {
-        if(y == 1 && x == 1) return true;
-        covered[y - 1][x - 1] = true;
+        if(y == r - 1 && x == c - 1) return true;
+        visited[y][x] = true;
 
-        for(int[] next : cells.get(y * x))
+        for(int[] coords : factors.get(room[y][x]))
         {
-            if(!covered[next[0] - 1][next[1] - 1])
-            {
-                covered[next[0] - 1][next[1] - 1] = true;
-                if(escapable(next[0], next[1])) return true;
-            }
+            if(visited[coords[0] - 1][coords[1] - 1]) continue;
+            if(search(coords[0] - 1, coords[1] - 1)) return true;
         }
 
         return false;
