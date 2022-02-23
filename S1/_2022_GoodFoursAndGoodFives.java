cb28a7a2530a9a36b0831a6fb0f4c8f0866b752c
Victor Yao <55715390+VictorVy@users.noreply.github.com>
2022-02-23 15:44:59 -0800

    Add files via upload

---
+++ S1/_2022_GoodFoursAndGoodFives.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _2022_GoodFoursAndGoodFives
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int count = 0;
+
+        for(int i = n / 5; i > 0; i--)
+            if(i * 5 == n || (n - i * 5) % 4 == 0) count++;
+
+        if(n % 4 == 0) count++;
+
+        System.out.println(count);
+    }
+}
